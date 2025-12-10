868c8720d83f634bde08da68fc28c2f3fd215faa
Triunix <kquiroz8379@utm.edu.ec>
2025-12-09 22:38:59 -0500

    agregando workflow CI/CD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/DesplegesUTM/integracion', // <- CAMBIA este nombre por el repo real en GitHub
+  base: '/integracion/', // MUY IMPORTANTE, EXACTO como el nombre del repo en GitHub
 });
